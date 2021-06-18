3f03085fb08c95670f62857c263fc7fd173c2d0d
Yonathan Tesfaye <85513618+Yonathan-Tesfaye-Mulu@users.noreply.github.com>
2021-06-18 21:46:18 +0300

    Update EthiopianCalendar.php

--- EthiopianCalendar.php
+++ EthiopianCalendar.php
@@ -2,7 +2,7 @@
 /*
  * Ethiopian Calendar v1.0 : https://github.com/ytDevelopment-ET/EthiopianCalendar
  *
- * Copyright (©) 2019-2020 ytDevelopment
+ * Copyright (©) 2019-2021 ytDevelopment
  * http://ytdevelopment.rf.gd/
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
